d702bed97acdc22ee645a331a5258bd7a87dc3f2
Allen Horine <allen.horine@gmail.com>
2024-02-21 01:36:00 -0500

    Added mappings for Trouble control

--- nvim/nvim/lua/config/mappings.lua
+++ nvim/nvim/lua/config/mappings.lua
@@ -1,5 +1,5 @@
 local map = vim.keymap.set
-local wk = require('which-key')
+local wk = require("which-key")
 -- General mappings
 map("n", ";", ":", { noremap = true })
 map("i", "jk", "<ESC>", { noremap = true })
@@ -10,40 +10,40 @@ map("n", "<C-k>", "<C-w>k", { noremap = true })
 map("n", "<C-l>", "<C-w>l", { noremap = true })
 -- Clear search highlights
 wk.register({
-  ['<space><space>'] = { ':nohlsearch<CR>', 'Clear highlights' },
+  ["<space><space>"] = { ":nohlsearch<CR>", "Clear highlights" },
 })
 -- OS clipboard macros
 -- Normal
 wk.register({
-  y = { '"+y', 'Yank to OS clipboard' },
-  yy = { '"+yy', 'Yank line to OS clipboard' },
-  Y = { '"+yg_', 'Yank to end of line to OS clipboard' },
-  p = { '"+p', 'Paste from OS clipboard' },
-  P = { '"+P', 'Paste from OS clipboard behind cursor' },
-}, { prefix = '<leader>' })
+  y = { '"+y', "Yank to OS clipboard" },
+  yy = { '"+yy', "Yank line to OS clipboard" },
+  Y = { '"+yg_', "Yank to end of line to OS clipboard" },
+  p = { '"+p', "Paste from OS clipboard" },
+  P = { '"+P', "Paste from OS clipboard behind cursor" },
+}, { prefix = "<leader>" })
 -- Visual
 wk.register({
-  y = { '"+y', 'Yank selection to OS clipboard' },
-  p = { '"+p', 'Paste to selection from OS clipboard' },
-  P = { '"+P', 'Paste to selection from OS clipboard' }, -- ??
+  y = { '"+y', "Yank selection to OS clipboard" },
+  p = { '"+p', "Paste to selection from OS clipboard" },
+  P = { '"+P', "Paste to selection from OS clipboard" }, -- ??
 }, {
-  mode = 'v',
-  prefix = '<leader>',
+  mode = "v",
+  prefix = "<leader>",
 })
 
 -- Plugins
 -- - Telescope
 wk.register({
   f = {
-    name = '+Telescope',
-    f = { '<cmd>Telescope find_files<cr>', 'Find file' },
-    a = { '<cmd>Telescope find_files hidden=true<cr>', 'Find file (+hidden)' },
-    g = { '<cmd>Telescope live_grep<cr>', 'Live grep' },
-    b = { '<cmd>Telescope buffers<cr>', 'Find buffer' },
-    h = { '<cmd>Telescope help_tags<cr>', 'Help tags' },
-    n = { '<cmd>Telescope notify<cr>', 'Notifications' },
+    name = "+Telescope",
+    f = { "<cmd>Telescope find_files<cr>", "Find file" },
+    a = { "<cmd>Telescope find_files hidden=true<cr>", "Find file (+hidden)" },
+    g = { "<cmd>Telescope live_grep<cr>", "Live grep" },
+    b = { "<cmd>Telescope buffers<cr>", "Find buffer" },
+    h = { "<cmd>Telescope help_tags<cr>", "Help tags" },
+    n = { "<cmd>Telescope notify<cr>", "Notifications" },
   },
-}, { prefix = '<leader>' })
+}, { prefix = "<leader>" })
 -- - Winshift
 map("n", "<C-w><C-m>", "<cmd>WinShift<cr>", { noremap = true })
 -- - LuaSnip
@@ -53,17 +53,28 @@ map("i", "<C-H>", "<cmd>lua require('luasnip').jump(-1)<cr>", { silent = true })
 -- - Scissors
 wk.register({
   s = {
-    name = 'Scissors',
-    e = { function() require('scissors').editSnippet() end, 'Edit snippet', },
-    a = { function() require('scissors').addNewSnippet() end, 'Add snippet', mode = { 'n', 'x' }, },
+    name = "Scissors",
+    e = {
+      function()
+        require("scissors").editSnippet()
+      end,
+      "Edit snippet",
+    },
+    a = {
+      function()
+        require("scissors").addNewSnippet()
+      end,
+      "Add snippet",
+      mode = { "n", "x" },
+    },
   },
-}, { prefix = '<leader>' })
+}, { prefix = "<leader>" })
 -- - Venn
 function _G.Toggle_venn()
   local venn_enabled = vim.inspect(vim.b.venn_enabled)
   if venn_enabled == "nil" then
     vim.b.venn_enabled = true
-    vim.cmd [[setlocal ve=all]]
+    vim.cmd([[setlocal ve=all]])
     -- draw a line on HJKL keystokes
     vim.api.nvim_buf_set_keymap(0, "n", "J", "<C-v>j:VBox<CR>", { noremap = true })
     vim.api.nvim_buf_set_keymap(0, "n", "K", "<C-v>k:VBox<CR>", { noremap = true })
@@ -72,20 +83,30 @@ function _G.Toggle_venn()
     -- draw a box by pressing "f" with visual selection
     vim.api.nvim_buf_set_keymap(0, "v", "f", ":VBox<CR>", { noremap = true })
   else
-    vim.cmd [[setlocal ve=]]
-    vim.cmd [[mapclear <buffer>]]
+    vim.cmd([[setlocal ve=]])
+    vim.cmd([[mapclear <buffer>]])
     vim.b.venn_enabled = nil
   end
 end
 
 -- toggle keymappings for venn using <leader>v
 wk.register({
-  ['<leader>v'] = { '<cmd>lua Toggle_venn()<cr>', 'Toggle Venn mode' },
+  ["<leader>v"] = { "<cmd>lua Toggle_venn()<cr>", "Toggle Venn mode" },
 })
 -- Buffer manager
 wk.register({
-  ['<leader>b'] = { require('buffer_manager.ui').toggle_quick_menu, 'Buffer manager' },
+  ["<leader>b"] = { require("buffer_manager.ui").toggle_quick_menu, "Buffer manager" },
 })
+-- Trouble
+wk.register({
+  t = {
+    name = "+Trouble",
+    t = { "<cmd>TroubleToggle<cr>", "Toggle trouble" },
+    o = { "<cmd>Trouble<cr>", "Open trouble" },
+    c = { "<cmd>TroubleClose<cr>", "Close trouble" },
+    r = { "<cmd>TroubleRefresh<cr>", "Refresh trouble" },
+  },
+}, { prefix = "<leader>" })
 
 -- - LSP
 -- Global mappings.
